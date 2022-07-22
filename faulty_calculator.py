0ae0d0333f8c2dfaf48ec2c66866e4db6017d25a
rahul <rahulktr.singh@gmail.com>
2022-07-22 15:06:37 +0530

    Correction

--- faulty_calculator.py
+++ faulty_calculator.py
@@ -1,30 +1,23 @@
-#45*3 = 555, 56+9 = 77, 56/6 = 4
-#Faulty calculator
-num1 = int(input("enter the first number "))
-num2 = int(input("enter the second number "))
-oper = input("enter the operator +,/,*,-,% ")
-if num1 == 45 and num2 == 3 and oper == "*" :
-    print(" 555")
-elif num1 == 56 and num2 == 9 and oper == "+" :
-    print(" 77")
-elif num1 == 56 and num2 == 6 and oper== "/"  :
-    print(" 4 ")
+# faulty calculator
+#Design a calculator which will correctly solve all the problems except these:  45 *3 = 555, 56+9 = 77, 56/6 = 4
+#program should take operator and the two inputs from th user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+oper = input("Select any from the given operators: *,/,+,- : ")
+
+if (num1 == 45 and num2 == 3 and oper == "*")or (num1 == 3 and num2 == 45 and oper == "*") :
+    print("555")
+elif (num1 == 56 and num2 == 9 and oper == "+") or (num1 == 9 and num2 == 56 and oper == "+"):
+    print("77")
+elif (num1 == 56 and num2 == 6 and oper == "/") :
+    print("4")
+elif oper == "*" :
+    print("Multiplication is: ",(num1*num2))
+elif oper == "+" :
+    print("sum is: ",(num1+num2))
 elif oper == "-" :
-    subt = num1 - num2
-    print(subt)
-elif oper == "+":
-    add = num1 + num2
-    print(add)
-elif oper == "*":
-    mult = num1 * num2
-    print(mult)
-elif oper == "%":
-    modu = num1 % num2
-    print(modu)
-elif oper == "/":
-    div = num1 / num2
-    print(div)
-else :
-    print("Error please recheck your entry")
-
-
+    print("Substraction is: ",(num1-num2))
+elif oper == "/" :
+    print("Division is: ",(num1/num2))
+else:
+    print("Please recheck your input")
